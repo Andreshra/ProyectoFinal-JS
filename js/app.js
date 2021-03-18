6a0be3cf8f0921ac1cbfbac7f82c994738a09832
Andreshra <jandresh98@gmail.com>
2021-03-18 15:02:42 -0300

    correccion

--- js/app.js
+++ js/app.js
@@ -1,37 +1,35 @@
-debugger
-let servicio = prompt("Ingrese el tipo de Web que desea");
-let seccion = parseInt(prompt("Ingrese el numero de secciones que desea en la web"));
-let precioSec = 500;
+let servicio1 = document.getElementById("radios1"),
+  servicio2 = document.getElementById("radios2"),
+  servicio3 = document.getElementById("radios3"),
+  confirmar = document.getElementById("confirmar");
+  
+//                                                Objetos
+
+const tiposWebs = [ {tipo: "institucional", precio: 300}, {tipo: "eCommerce", precio: 300}, {tipo: "cotizador", precio: 700} ];
 
 
-//Valor de servicios en objetos
+class Web {
+  constructor(tipo, precio) {
+    this.tipo = tipo;
+    this.precio = precio;
 
-//Se calcula el N. de secciones por el precio unitario de cada seccion.
-let totalSec = seccion * precioSec;
-console.warn(totalSec);
+  }
+  mostrarWeb() {
+      if (servicio1.checked) {
+      alert(`Has seleccionado un diseño web de tipo ${this.tipo} , la cual tiene un precio de $${this.precio}.`)
+        }  else if (servicio2.checked) {
+        alert(`Has seleccionado un diseño web de tipo ${this.tipo} , la cual tiene un precio de $${this.precio}.`)
+        } else if (servicio3.checked) {
+        alert(`Has seleccionado un diseño web de tipo ${this.tipo} , la cual tiene un precio de $${this.precio}.`)
+        } else 
+        alert("Por favor, seleccione un servicio");
+    }
+}
 
+const servi1 =  new Web('institucional', 300);
+const servi2 = new Web ('eCommerce', 500);
+const servi3 = new Web ('cotizador', 700);
 
-//Ciclo if
-if (servicio == "institucional") {
-    let precioFinal = totalSec + 1500
-    alert("Ha seleccionado Diseño web institucional y tiene un costo de $" + precioFinal);
-} else if (servicio == "eCommerce") {
-    let precioFinal = totalSec + 1300
-    alert("Ha seleccionado Diseño web de eCommerce y tiene un costo de $" + precioFinal);
-} else if (servicio == "cotizador") {
-    let precioFinal = totalSec + 1700
-    alert("Ha seleccionado Diseño web de cotizador de productos o servicios y tiene un costo de $" + precioFinal);
-} else {
-    console.error("Este servicio no esta disponible")
-};
-
-
-
-
-
-
-
-
-
-//Se calcula el total del servicio selecionado
-//let monto = parseInt(prompt("Ingrese el monto"));
+servi1.mostrarWeb();
+servi2.mostrarWeb();
+servi3.mostrarWeb();
