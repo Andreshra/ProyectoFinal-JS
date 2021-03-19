90038feba79d1a170e4f92589c41c799afb81e22
Andreshra <jandresh98@gmail.com>
2021-03-18 23:02:08 -0300

    Correcion para re-Entrega

--- js/app.js
+++ js/app.js
@@ -5,31 +5,40 @@ let servicio1 = document.getElementById("radios1"),
   
 //                                                Objetos
 
-const tiposWebs = [ {tipo: "institucional", precio: 300}, {tipo: "eCommerce", precio: 300}, {tipo: "cotizador", precio: 700} ];
+const tiposWebs = [ {tipo: "institucional", precio: 1300}, {tipo: "eCommerce", precio: 1300}, {tipo: "cotizador", precio: 1700} ];
 
 
 class Web {
   constructor(tipo, precio) {
     this.tipo = tipo;
-    this.precio = precio;
-
+    this.precio = precio
   }
   mostrarWeb() {
       if (servicio1.checked) {
-      alert(`Has seleccionado un diseño web de tipo ${this.tipo} , la cual tiene un precio de $${this.precio}.`)
-        }  else if (servicio2.checked) {
-        alert(`Has seleccionado un diseño web de tipo ${this.tipo} , la cual tiene un precio de $${this.precio}.`)
-        } else if (servicio3.checked) {
-        alert(`Has seleccionado un diseño web de tipo ${this.tipo} , la cual tiene un precio de $${this.precio}.`)
-        } else 
-        alert("Por favor, seleccione un servicio");
+      alert(`Has seleccionado un diseño web de tipo ${this.tipo}, la cual tiene un precio de $${this.precio}.`)
+        }
+      if (!servicio1.checked && !servicio2.checked && !servicio3.checked) 
+      alert("Por favor, seleccione un servicio");
     }
+  mostrarWeb1() {
+      if (servicio2.checked) {
+      alert(`Has seleccionado un diseño web de tipo ${this.tipo}, la cual tiene un precio de $${this.precio}.`)
+      } 
+  }
+  mostrarWeb2() {
+    if (servicio3.checked) {
+      alert(`Has seleccionado un diseño web de tipo ${this.tipo}, la cual tiene un precio de $${this.precio}.`)
+      }
+  }
+
 }
 
-const servi1 =  new Web('institucional', 300);
-const servi2 = new Web ('eCommerce', 500);
-const servi3 = new Web ('cotizador', 700);
+const servi1 =  new Web('institucional', 1300);
+const servi2 = new Web ('eCommerce', 1500);
+const servi3 = new Web ('cotizador', 1700);
 
-servi1.mostrarWeb();
-servi2.mostrarWeb();
-servi3.mostrarWeb();
+confirmar.onclick = () => {
+  servi1.mostrarWeb()
+  servi2.mostrarWeb1()
+  servi3.mostrarWeb2()
+}
