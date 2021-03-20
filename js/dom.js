f032de51881df911b55805009bf02d369541633e
Andreshra <jandresh98@gmail.com>
2021-03-20 00:09:04 -0300

    Desafio entregable Json y DOM

---
+++ js/dom.js
@@ -0,0 +1,31 @@
+//Añadiendo texto mediante JavaScript
+const titulo = document.getElementById("titulo")
+
+function construirDOM(elemento, ubicacion) {
+    ubicacion.appendChild(elemento)
+}
+
+//encabezado.innerHTML = ""
+
+const tituloDom = document.createElement("p");
+    tituloDom.innerText = "Bienvenido a nuestro cotizador de servicios Web.";
+    tituloDom.classList.add("fst-italic")
+    construirDOM(tituloDom, titulo)
+
+    // Formulario de datos
+
+const nombre = document.getElementById("name")
+const email = document.getElementById("email")
+const enviarDatos = document.getElementById("enviarDatos")
+
+// Funcion que obtiene datos y los almacena en LocalStorage
+function cargarDatos() {
+    let info = {nombre: "", email: ""}
+    info.nombre = nombre.value
+    info.email = email.value
+
+    localStorage.setItem('Nombre', nombre.value)
+    localStorage.setItem('email', email.value)
+}
+
+enviarDatos.onclick = cargarDatos
