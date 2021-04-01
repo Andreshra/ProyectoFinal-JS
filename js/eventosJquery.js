28452fc20712c844203ac59f95f9323cf9d3f44e
Andreshra <jandresh98@gmail.com>
2021-04-01 18:26:12 -0300

    Mas codigo agregado. jQuery

--- js/eventosJquery.js
+++ js/eventosJquery.js
@@ -4,9 +4,15 @@ $(document).ready(function() {
 
     $("#enviarDatos") .click ( () => {
         let divFormDatos = $("#formDatos")
-        divFormDatos.append ('<div class="mx-auto" style="width: 200px;"> <h4 class="textoJq" > Se han cargado los datos correctamente.</h4> </div>')
+        if ($("#nombre").val() === "" && $("#email").val() === "") {
+            divFormDatos.append ('<div class="mx-auto" style="width: 200px; color: red;"> <h4> Campos vacios.</h4> </div>')
+            console.log("No se cargó ningún dato.")
+            return false
+        } else { 
+            divFormDatos.append ('<div class="mx-auto" style="width: 200px;"> <h4 class="textoJq" > Se han cargado los datos correctamente.</h4> </div>')
+            console.log("Datos cargados en LocalStorage")
+        }
         
-        console.log("Datos cargados en LocalStorage")
     })
 
 });
