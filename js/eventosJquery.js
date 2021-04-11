5414f9f151f5b0e02e6dca2321ae856195a1c7d3
Andreshra <jandresh98@gmail.com>
2021-04-11 19:36:24 -0400

    Correccion duplicacion de texto en formulario

--- js/eventosJquery.js
+++ js/eventosJquery.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+/* $(document).ready(function() {
     console.log('DOM is ready!');
 
 
@@ -16,4 +16,23 @@ $(document).ready(function() {
         } 
     })
 
+}); */
+
+$(document).ready(function() {
+    console.log('DOM is ready!');
+    $("#enviarDatos") .click ( () => {
+        let divFormDatos = $("#divBotonEnviarForm")
+        if($("#txtCampoVacio")) { $("#txtCampoVacio").remove(); }
+        if ($("#nombre").val() === "" && $("#email").val() === "") {
+            divFormDatos.append ('<h4 id="txtCampoVacio"> Campos vacios.</h4>')
+            $("#txtCampoVacio").fadeOut(3000)
+            console.log("No se cargó ningún dato.")
+            return false
+        } else { 
+            if($(".textoJq")) { $(".textoJq").remove(); }
+            divFormDatos.append ('<h4 class="textoJq"> Se han cargado los datos correctamente.</h4>')
+            $(".textoJq").fadeOut(3000)
+            console.log("Datos cargados en LocalStorage")
+        } 
+    })
 });
